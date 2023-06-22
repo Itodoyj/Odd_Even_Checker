86fded92eaf5148037e3f707315e65eba42e682c
yitod <itodoyj@mail.uc.edu>
2023-06-21 20:49:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 27; // Hardcoded input number
+
+        String result = (number % 2 == 0) ? "even" : "odd";
+        System.out.println("The number " + number + " is " + result);
+    }
+}
